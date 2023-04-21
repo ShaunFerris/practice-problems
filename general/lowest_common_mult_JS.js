154ef633750c5b0965e94ac20ac79b7ce6af1313
Shaun Ferris <slb.ferris46@gmail.com>
2023-04-21 13:09:44 +0100

    reorganization

---
+++ general/lowest_common_mult_JS.js
@@ -0,0 +1,11 @@
+/*
+Write a function to find the lowest common multiple for
+all numbers in a range between the two numbers provided in
+an array.
+*/
+
+const testArray = [1, 5];
+
+function lcm(arr) {
+     
+}
