58c0216d3d0f67581f07666f81ac5647351e47f3
Shaun Ferris <slb.ferris46@gmail.com>
2023-07-19 10:36:23 +0200

    Fixed issue in python valid anagrams solutions

--- general/valid_anagrams_python.py
+++ general/valid_anagrams_python.py
@@ -1,29 +1,37 @@
-'''
+"""
 Create a function that takes two inputs and returns true if they are anagrams
 or false if they are not.
-'''
+"""
 import time
 
-w1 = 'garden'
-w2 = 'danger'
+w1 = "garden"
+w2 = "danger"
 
-#Solution one, my blind attempt at solving this problem.
+
+# Solution one, my blind attempt at solving this problem.
 def is_anagram(w1, w2):
     let_list1, let_list2 = [*w1], [*w2]
+    memo = []
     if len(let_list1) != len(let_list2):
         return False
     else:
         for i in let_list1:
-            if i not in let_list2:
+            if i not in memo:
+                memo.append(i)
+            else:
+                continue
+            if let_list1.count(i) != let_list2.count(i):
                 return False
         return True
+
 
 start = time.time()
 print(is_anagram(w1, w2))
 end = time.time()
-print(f'Function is_anagram completed in {(end - start)*1000} ms.')
+print(f"Function is_anagram completed in {(end - start)*1000} ms.")
 
-#Solution two, using dictionaries
+
+# Solution two, using dictionaries
 def dict_anagram(w1, w2):
     if len(w1) != len(w2):
         return False
@@ -32,7 +40,8 @@ def dict_anagram(w1, w2):
         w2_dict = {c: w2.count(c) for c in w2}
         return w1_dict == w2_dict
 
+
 start = time.time()
 print(dict_anagram(w1, w2))
 end = time.time()
-print(f'Function dict_anagram completed in {(end - start)*1000} ms.')
+print(f"Function dict_anagram completed in {(end - start)*1000} ms.")
